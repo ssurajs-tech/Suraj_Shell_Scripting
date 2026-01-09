2ad07a0ac7ad0edba89c47b5ab63f26273ae0922
ssurajs-tech <ssurajs1008@gmail.com>
2026-01-09 09:38:15 +0530

    modified the code

--- array/adding_array.sh
+++ array/adding_array.sh
@@ -2,7 +2,20 @@
 
 distros=("Ubuntu" "Red Hat" "Fedora" "Debian")
 echo ${distros[*]}
+echo " "
 
 # Adding element to the array
 distros+=("kali")
+
 echo ${distros[*]}
+
+echo "Difference between \${array[*]} and \${array[@]}"
+echo " "
+
+for distro in "${distros[*]}"; do
+    echo "how it behaves: $distro"
+done
+
+for item in "${distros[@]}"; do
+    echo "How it behaves: $item"
+done
