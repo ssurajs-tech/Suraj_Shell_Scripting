9a0578455284c771c7add0049fd42aa2f628474b
ssurajs-tech <ssurajs1008@gmail.com>
2026-01-12 12:10:36 +0530

    added a content to the repo

--- array/array_elements.sh
+++ array/array_elements.sh
@@ -16,3 +16,7 @@ files=("f1.txt" "f2.txt" "f3.txt" "f4.txt" "f5.txt")
 
 echo ${files[1]}
 echo ${files[2]}
+
+echo "All files: ${files[*]}"
+echo "All files using @: ${files[@]}"
+echo "Total number of files: ${#files[@]}"
