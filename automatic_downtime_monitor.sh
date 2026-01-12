e52e9cd735672672ec1d3cee603ad8fcdd700367
ssurajs-tech <ssurajs1008@gmail.com>
2026-01-12 12:15:12 +0530

    added a content to the repo

--- automatic_downtime_monitor.sh
+++ automatic_downtime_monitor.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Stored the list of the of the server in the array for fixed number of servers. 
+
 serverlist=( servera.lab.example.com serverb.lab.example.com )
 
+# Loop through the array and ping each server, if the server is unreachable send an email notification.
 for i in ${serverlist[@]};
 do 
     ping -c1 $i >> /dev/null 2>&1
@@ -10,6 +13,8 @@ do
         echo "@i is unreachable" | mail -s $i surajss@gmail.com
     fi
 done
+
+# Alternatively, you can read the list of servers from a file named 'serverlist' and perform the same operation.
 
 servers=$(cat serverlist)
 for i in $servers;
